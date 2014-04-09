6bb834b6a5085a846a6014865fccbaec79345a8b
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2014-04-09 10:44:26 +0800

    Added guest controller, header file, and FontAwesome.

---
+++ application/views/common/header.php
@@ -0,0 +1,6 @@
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Tara | Let's work</title>
+	<link rel="stylesheet" href="<?PHP echo base_url(); ?>css/foundation.css" />
+	<link rel="stylesheet" href="<?PHP echo base_url(); ?>css/font-awesome.min.css">
+	<script src="<?PHP echo base_url(); ?>js/vendor/modernizr.js"></script>
