e1f9d1809d1666ac56c22c0452fd2bf311f3f117
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2014-04-18 18:37:26 +0800

    Proper links for registration and log in at navigation fixed.

--- application/views/common/login.php
+++ application/views/common/login.php
@@ -1,14 +1,16 @@
 <!-- Right Nav Section -->
 <ul class="right">
-	<?php if (false && $this->session->userdata('user_id') && $this->session->userdata('password')): ?>
-	<li class="active"><a href="#">Register</a></li>
-	<li>
-		<?php echo anchor('login', 'Log in'); ?>
-	</li>
-	<?php else: ?>
+	<?php if ($this->session->userdata('user_id') && $this->session->userdata('password')): ?>
 	<li class="active"><a href="#">darrensapalo</a></li>
 	<li>
 		<?php echo anchor('account/settings', 'Settings'); ?>
+	</li>
+	<?php else: ?>
+	<li class="active">
+		<?php echo anchor('account/register', 'Register'); ?>
+	</li>
+	<li>
+		<?php echo anchor('account/login', 'Log in'); ?>
 	</li>
 	<?php endif; ?>
 </ul>
